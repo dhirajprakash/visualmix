339ad068b4cbfd4c25abe3f259508a35effe2dfa
Dhiraj Prakash <dhiraj@brazhuman.com>
2018-11-11 19:19:29 -0200

    Hibernate Minor changes

--- src/main/java/com/dhirajprakash/vm/config/HibernateConfiguration.java
+++ src/main/java/com/dhirajprakash/vm/config/HibernateConfiguration.java
@@ -1,10 +1,15 @@
 package com.dhirajprakash.vm.config;
 
+import java.util.List;
 import java.util.Properties;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
+import org.hibernate.Criteria;
+import org.hibernate.SQLQuery;
 import org.hibernate.SessionFactory;
+import org.hibernate.transform.Transformers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +22,12 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+
+import srikant.shakti.model.PlacaDetails;
+import srikant.shakti.model.ReportMetadata;
 
 @Configuration
 @EnableTransactionManagement
@@ -89,4 +100,19 @@ public class HibernateConfiguration {
 		return properties;
 	}
 
+	/*
+	 * @RequestMapping(value = { "getDataWeakely"}, method = RequestMethod.POST)
+	 * public List<ReportMetadata> getDataWeakely(HttpServletResponse
+	 * response,WebRequest webRequest){ SQLQuery q =
+	 * sessionFactory.getCurrentSession().
+	 * createSQLQuery("select occurencia_day,count(occurencia_day) from report_metadata group by occurencia_day;"
+	 * ); return q.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP).list(); }
+	 * 
+	 * @RequestMapping(value = { "getpleacedetail"}, method = RequestMethod.POST)
+	 * public List<PlacaDetails> getPleaceDetail(HttpServletResponse
+	 * response,WebRequest webRequest){ Criteria cr =
+	 * sessionFactory.getCurrentSession().createCriteria(PlacaDetails.class);
+	 * List<PlacaDetails> pleaceDetail = cr.list(); return pleaceDetail; }
+	 */
+	*/
 }
